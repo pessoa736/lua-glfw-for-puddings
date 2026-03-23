639093a508c5ff078004265545f4ed4da212fda2
Davi <69681778+pessoa736@users.noreply.github.com>
2026-03-23 09:23:31 -0300

    add typedef to lua with lualanguageserver

---
+++ library/lgfp.lua
@@ -0,0 +1,21 @@
+---@meta lgfp
+---@module "lgfp"
+---@autor Davi passos / pessoa736 / xpudding
+---
+---@class lgfpLibDef
+--- lua glfw for puddings
+local lgfp = {}
+
+---@return boolean isInitialized
+lgfp.IsInitialized = function () end
+
+
+
+---@param width integer
+---@param height integer
+---@param title string
+---@return userdata
+lgfp.CreateWindows = function (width, height, title) end
+
+
+return lgfp
