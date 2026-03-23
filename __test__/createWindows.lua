95d9ecd892df94519a2a0bfcf05f4c033a7ce71f
Davi <69681778+pessoa736@users.noreply.github.com>
2026-03-23 10:54:26 -0300

    refactor: simplify window manager reference in createWindows.lua

--- __test__/createWindows.lua
+++ __test__/createWindows.lua
@@ -1,8 +1,9 @@
 package.cpath =  "./build/?.so;" .. package.cpath
 
 local lgfp = require("lgfp")
+local winman = lgfp.WindowManager
 
-local win = lgfp.WindowManager.CreateWindows(800, 600, "teste de criação de function")
+local win = winman.CreateWindows(800, 600, "teste de criação de function")
 
 if win:ShouldClose() then
     print("close window")
