56bbf5ef8a9520704867b0419da72e6d6cb9a963
Davi <69681778+pessoa736@users.noreply.github.com>
2026-03-24 22:36:24 -0300

    refactor GLFW initialization and window management functions.

--- src/setupLua.c
+++ src/setupLua.c
@@ -6,32 +6,16 @@
 #include "panic.h"
 
 
-int LlgfpIsInitialized(lua_State *L){
+int LGLFWInit(lua_State *L){
     int res = glfwInit();
-    lua_pushboolean(L, res);
+    lua_pushboolean(L, (int)res);
     return 1;
 }
 
-
-int LCreateWindowsMetatable(lua_State *L){
-    luaL_newmetatable(L, "LGFP.WindowManager");
-
-    lua_pushvalue(L, -1);
-
-    lua_setfield(L, -2, "__index");
-
-    lua_pushcfunction(L, LMakeCurrentWindows);
-    lua_setfield(L, -2, "makeContextCurrent");
-
-    lua_pushcfunction(L, LCloseWindows);
-    lua_setfield(L, -2, "__gc");
-
-    lua_pushcfunction(L, LShouldCloseWindows);
-    lua_setfield(L, -2, "ShouldClose");
-
-    lua_pop(L, 1);
+int LTerminateGLFW(lua_State *L){
+    glfwTerminate();
+    return 0;
 }
-
 
 
 
@@ -44,13 +28,18 @@ static LR Lfunctions [] ={
 
 
 int luaopen_lgfp(lua_State* L){
-    PANIC(L, !glfwInit(), "GLFW not initialized or not instaled");
     LCreateWindowsMetatable(L);
 
     lua_newtable(L);
 
     luaL_newlib(L, Lfunctions);
     lua_setfield(L, -2, "WindowManager");
+
+    lua_pushcfunction(L, LGLFWInit);
+    lua_setfield(L, -2, "Init");
+    lua_pushcfunction(L, LTerminateGLFW);
+    lua_setfield(L, -2, "Terminate");
+
 
     return 1;
 }
--- src/window.c
+++ src/window.c
@@ -1,4 +1,6 @@
-
+#include <string.h>
+#include <malloc.h>
+#include <stdlib.h>
 #include <lua.h>
 #include <lualib.h>
 #include <lauxlib.h>
@@ -9,20 +11,34 @@
 
 
 
-int LCreateWindow(lua_State *L){
-    int width  = luaL_checknumber(L, 1);
-    int heigth = luaL_checknumber(L, 2);
+int LCreateWindow(lua_State *L)
+{
+    
+    LuaWindows *ud = (LuaWindows *)lua_newuserdata(L, sizeof(LuaWindows));
+
+    LuaPANIC(L, ud == NULL,"memory not allocated");
+    
+
+    ud->width  = luaL_checknumber(L, 1);
+    ud->heigth = luaL_checknumber(L, 2);
     
     string title = luaL_checkstring(L, 3);
+    ud->title = strdup(title);
+
+    LuaPANIC(L, !(ud->width>0), "expected width to be greater than 0. current width: %d", ud->width);
+    LuaPANIC(L, !(ud->heigth>0), "expected heigth to be greater than 0. current height: %d", ud->heigth);
+    LuaPANIC(L, !(neoTypeOf(ud->title)=="string"), "expected a string in 3# arg");
+        
     
-    GWindow win = glfwCreateWindow(width, heigth, title, NULL, NULL);
-
-    PANIC(L, !win, "failed to create GLFW windows");
-
-    LuaWindows ud = (LuaWindows )lua_newuserdata(L, sizeof(LuaWindows));
-    ud->window = win;
-
-    PANIC(L, !ud, "not created userdata");
+    GWindow win = glfwCreateWindow(ud->width, ud->heigth, ud->title, NULL, NULL);
+    
+    if (win){
+        ud->window = win;
+    }else{
+        glfwTerminate();
+        LuaPANIC(L, 1, "failed to create GLFW windows");
+        return 0;
+    }
 
     luaL_getmetatable(L, "LGFP.WindowManager");
     lua_setmetatable(L, -2);
@@ -31,19 +47,26 @@ int LCreateWindow(lua_State *L){
 }
 
 int LMakeCurrentWindows(lua_State *L){
-    LuaWindows ud = luaL_checkudata(L, 1, "LGFP.WindowManager");
+    LuaWindows *ud  = luaL_checkudata(L, 1, "LGFP.WindowManager");
 
     glfwMakeContextCurrent(ud->window);
 
     return 0;
 }
 
-int LCloseWindows(lua_State *L){
-    LuaWindows ud = luaL_checkudata(L, 1, "LGFP.WindowManager");
 
-    if (ud->window){
+//__gc
+int LCloseWindows(lua_State *L)
+{
+    LuaWindows *ud = (LuaWindows *)lua_newuserdata(L, sizeof(LuaWindows));
+    LuaPANIC(L, ud == NULL, "not get Window or no memory");
+    ud = luaL_checkudata(L, 1, "LGFP.WindowManager");
+
+    if (ud)
+    {
         glfwDestroyWindow(ud->window);
-        ud->window = NULL;
+        free(ud->title);
+        ud->title = NULL;
     }
 
     return 0;
@@ -51,11 +74,72 @@ int LCloseWindows(lua_State *L){
 
 
 int LShouldCloseWindows(lua_State *L){
-    LuaWindows ud = luaL_checkudata(L, 1, "LGFP.WindowManager");
+    LuaWindows *ud = luaL_checkudata(L, 1, "LGFP.WindowManager");
 
     int res = glfwWindowShouldClose(ud->window);
 
     lua_pushboolean(L, res);
 
     return 1;
+}
+
+
+int LGetWidthWindows(lua_State *L){
+    LuaWindows *ud = luaL_checkudata(L, 1, "LGFP.WindowManager");
+
+    LuaPANIC(L, !ud, "not found window")
+    
+    lua_pushinteger(L, (int)ud->width);
+
+    return 1;
+}
+
+int LGetHeightWindows(lua_State *L){
+    LuaWindows *ud = luaL_checkudata(L, 1, "LGFP.WindowManager");
+
+    LuaPANIC(L, !ud, "not found window")
+    
+    lua_pushinteger(L, (int)ud->heigth);
+
+    return 1;
+}
+
+int LGetTitleWindows(lua_State *L){
+    LuaWindows *ud = luaL_checkudata(L, 1, "LGFP.WindowManager");
+
+    LuaPANIC(L, !ud, "not found window")
+    
+    printf("%s\n", ud->title);
+    lua_pushstring(L, ud->title);
+
+    return 1;
+}
+
+
+int LCreateWindowsMetatable(lua_State *L)
+{
+    luaL_newmetatable(L, "LGFP.WindowManager");
+
+    lua_pushvalue(L, -1);
+    lua_setfield(L, -2, "__index");
+
+    lua_pushcfunction(L, LMakeCurrentWindows);
+    lua_setfield(L, -2, "MakeContextCurrent");
+
+    lua_pushcfunction(L, LCloseWindows);
+    lua_setfield(L, -2, "__gc");
+
+    lua_pushcfunction(L, LShouldCloseWindows);
+    lua_setfield(L, -2, "ShouldClose");
+
+    lua_pushcfunction(L, LGetWidthWindows);
+    lua_setfield(L, -2, "GetWidth");
+
+    lua_pushcfunction(L, LGetHeightWindows);
+    lua_setfield(L, -2, "GetHeight");
+
+    lua_pushcfunction(L, LGetTitleWindows);
+    lua_setfield(L, -2, "GetTitle");
+
+    lua_pop(L, 1);
 }
