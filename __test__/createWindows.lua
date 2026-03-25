4a4173f07ca0073f4b9e81a3578c1661787a2791
Davi <69681778+pessoa736@users.noreply.github.com>
2026-03-24 22:37:46 -0300

    refactor: test window creation logic and initialization flow

--- __test__/createWindows.lua
+++ __test__/createWindows.lua
@@ -1,12 +1,27 @@
 package.cpath =  "./build/?.so;" .. package.cpath
 
 local lgfp = require("lgfp")
-local winman = lgfp.WindowManager
 
-local win = winman.CreateWindows(800, 600, "teste de criação de function")
 
-if win:ShouldClose() then
-    print("close window")
+if (lgfp.Init()) then
+
+    local winman = lgfp.WindowManager
+
+    local win = winman.CreateWindows(600, 400, "teste de criação de function")
+    
+    if not win then
+        lgfp.Terminate()
+        error("Creation Windows failed")
+    end
+
+    win:MakeContextCurrent()
+
+    while not win:ShouldClose() do
+        print("close window")
+    end
+
+
+    print(win, type(win), win:GetWidth(), win:GetHeight(), win:GetTitle())
+    
+    lgfp.Terminate()
 end
-
-print(win)
