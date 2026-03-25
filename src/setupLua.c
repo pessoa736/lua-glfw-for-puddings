5a3f1a57f59ac3c4a00b64e5b438b49333387d06
Davi <69681778+pessoa736@users.noreply.github.com>
2026-03-25 10:44:15 -0300

    add PollEvent function

--- src/setupLua.c
+++ src/setupLua.c
@@ -17,6 +17,10 @@ int LTerminateGLFW(lua_State *L){
     return 0;
 }
 
+int LPollEvents(lua_State *L){
+    glfwPollEvents();
+    return 0;
+}
 
 
 static LR Lfunctions [] ={
@@ -39,6 +43,9 @@ int luaopen_lgfp(lua_State* L){
     lua_setfield(L, -2, "Init");
     lua_pushcfunction(L, LTerminateGLFW);
     lua_setfield(L, -2, "Terminate");
+    
+    lua_pushcfunction(L, LPollEvents);
+    lua_setfield(L, -2, "PollEvents");
 
 
     return 1;
