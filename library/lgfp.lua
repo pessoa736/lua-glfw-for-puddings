bf93bf8f697f658f7ee99a806dde3b28963c42fe
Davi <69681778+pessoa736@users.noreply.github.com>
2026-03-25 10:46:20 -0300

    update lua defs to add Destroy, Swapbuffers and PollEvents

--- library/lgfp.lua
+++ library/lgfp.lua
@@ -13,6 +13,9 @@ lgfp.Init = function ()end
 ---turn off ther GLFW
 lgfp.Terminate = function () end
 
+---update Events
+lgfp.PollEvents = function () end
+
 
 ---@class LuaWindows: userdata
 ---@field ShouldClose fun(self: LuaWindows):boolean
@@ -20,6 +23,8 @@ lgfp.Terminate = function () end
 ---@field GetWidth fun(self: LuaWindows):integer
 ---@field GetHeight fun(self: LuaWindows):integer
 ---@field GetTitle fun(self: LuaWindows):string 
+---@field Destroy fun(self: LuaWindows)
+---@field SwapBuffers fun(self: LuaWindows)
 
 
 ---@class lgfpWindowManagerLibDef: table, metatable
